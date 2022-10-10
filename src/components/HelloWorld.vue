44c9f96462f13abe66af72408a1d6e761d94522b
JoseQuirosDev <josequirosdev@gmail.com>
2022-10-10 13:52:00 -0600

    Prueba 15000

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,10 +12,7 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-  <a target="_blank" href="https://vuejs.org/">?Perro</a>.
-  <a target="_blank" href="https://vuejs.org/">?Perro</a>.
+      <a target="_blank" href="https://vitejs.dev/">Vite</a>
     </h3>
   </div>
 </template>
